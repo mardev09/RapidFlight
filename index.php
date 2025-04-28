7d84fe42540dfa2ee20953cf33a0ff833a0ef546
Omar <152691870+omaralimoh@users.noreply.github.com>
2025-04-28 15:59:20 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CabReserves | Billetes de vuelos y descuentos</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body class="">
+    <nav class="">
+        <a href="./">
+            <img src="./assets/img/logo.png" alt="">
+        </a>
+        <ul class="">
+            <a href=""><li>Rutas y puertos</li></a>
+            <a href=""><li>Reservas</li></a>
+            <a href=""><li>Contacto</li></a>
+            <span></span>
+            <a class="nav-reserve-now" href=""><li>
+                Reservar ahora
+                <i class="hgi hgi-stroke hgi-flying-human"></i>
+            </li></a>
+            <span></span>
+            <a href=""><li>
+                <i class="hgi hgi-stroke hgi-menu-01"></i>
+                <i class="hgi hgi-stroke hgi-user-circle-02"></i>
+            </li></a>
+        </ul>
+    </nav>
+    <main>
+        <section class="landing">
+        <div class="bg-gradient"></div>
+        
+
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
