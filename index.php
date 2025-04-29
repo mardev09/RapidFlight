b503c90eb26cb17dd9dd0b2bff94a019eb1dca77
Omar <152691870+omaralimoh@users.noreply.github.com>
2025-04-29 17:31:23 +0200

    Diseño searcher terminado

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <title>CabReserves | Billetes de vuelos y descuentos</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
+    <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css"
+    >
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body class="">
@@ -33,30 +37,80 @@
         <section class="landing">
         <div class="bg-gradient"></div>
         <h1>Bienvenido a CabReserves: Tu portal de billetes de vuelo</h1>
-        <div class="landing-search">
-            <div class="landing-search-type">
+        <div class="search">
+            <div class="search-type">
                 <button autofocus>Solo ida</button>
                 <button>Ida y vuelta</button>
             </div>
-            <div class="landing-search-searcher">
-                <div class="landing-search-from-to">
-                    <span class="landing-search-select">
+            <div class="search-searcher">
+                <div class="search-searcher-row">
+                    <div class="search-from-to">
+                        <span class="search-select">
+                            <span>
+                                <p>Origen</p>
+                                <p class="unselected">¿De dónde sales?</p>
+                            </span>
+                            <i class="hgi hgi-stroke hgi-sharp hgi-arrow-down-01"></i>
+                        </span>
+                        <button class="changeBtn">
+                            <i class="hgi hgi-stroke hgi-repeat"></i>
+                        </button>
+                        <span class="search-select">
+                            <span>
+                                <p>Destino</p>
+                                <p class="unselected">¿A dónde vas?</p>
+                            </span>
+                            <i class="hgi hgi-stroke hgi-sharp hgi-arrow-down-01"></i>
+                        </span>
+                    </div>
+                    <span class="search-select">
                         <span>
-                            <p>Origen</p>
-                            <p>¿De dónde sales?</p>
+                            <p>Ida</p>
+                            <p>29/4/2025</p>
                         </span>
-                        <i class="hgi hgi-stroke hgi-sharp hgi-arrow-down-01"></i>
+                        <i class="hgi hgi-stroke hgi-calendar-03"></i>
                     </span>
-                    <button class="change">
-                        <i class="hgi hgi-stroke hgi-repeat"></i>
+                    <span class="search-select">
+                        <span>
+                            <p>Vuelta</p>
+                            <p>01/5/2025</p>
+                        </span>
+                        <i class="hgi hgi-stroke hgi-calendar-03"></i>
+                    </span>
+                    <span class="search-select">
+                        <span>
+                            <p>Pasajeros</p>
+                            <p>1 pasajero</p>
+                        </span>
+                        <i class="fa-solid fa-person"></i>
+                    </span>
+                    <button class="searchBtn">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <p>Buscar</p>
                     </button>
-                    <span class="landing-search-select">
-                    <span>
-                        <p>Destino</p>
-                        <p>¿A dónde vas?</p>
-                        </span>
-                        <i class="hgi hgi-stroke hgi-sharp hgi-arrow-down-01"></i>
-                    </span>
+                </div>
+                
+                <div class="search-others">
+                    <button class="othersBtn">
+                        <i class="fa-solid fa-car"></i>
+                        Vehículo
+                    </button>
+                    <button class="othersBtn">
+                        <i class="fa-solid fa-paw"></i>
+                        Mascotas
+                    </button>
+                    <button class="othersBtn">
+                        <i class="fa-solid fa-users"></i>
+                        Familia numerosa
+                    </button>
+                    <button class="othersBtn">
+                        <i class="fa-solid fa-medal"></i>
+                        Bonificación
+                    </button>
+                    <button class="othersBtn">
+                        <i class="fa-solid fa-gift"></i>
+                        Descuentos
+                    </button>
                 </div>
             </div>
         </div>
