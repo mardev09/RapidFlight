81b30f2dece0844d9c42e74a1b14367e470d0c89
mardev <omaralianmohamed0099@gmail.com>
2026-05-13 01:14:50 +0200

    Actualizados los CDN de FA Pro

--- src/Templates/head.inc.php
+++ src/Templates/head.inc.php
@@ -3,7 +3,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RapidFlight | Billetes de vuelos y descuentos</title>
     <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
-    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css">
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/fontawesome.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/sharp-solid.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/sharp-regular.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/sharp-light.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/duotone.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/brands.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/solid.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/regular.css"
+    />
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/rizmyabdulla/fontawesome-pro@main/releases/v6.7.2/css/light.css"
+    />
     <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="public/css/media.css">
